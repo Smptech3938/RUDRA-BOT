c8484f9de108b628d9ee1302c80ebf2247212f1c
SMP_TG <93422140+Smptech3938@users.noreply.github.com>
2025-01-16 18:01:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -407,7 +407,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
             InlineKeyboardButton('🔎 Search 🔍', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('♻️ Group ♻️', url='https://t.me/TheBioscope')
+            InlineKeyboardButton('♻️ Group ♻️', url='https://t.me/The_Bioscope')
         ], [
             InlineKeyboardButton('🤔 Help ', callback_data='help'),
             InlineKeyboardButton('♥️ About ', callback_data='about')
@@ -438,7 +438,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about":
         buttons = [[
-            InlineKeyboardButton('♻️ Group ♻️', url='https://t.me/TheBioscope'),
+            InlineKeyboardButton('♻️ Group ♻️', url='https://t.me/https://t.me/The_Bioscope'),
             InlineKeyboardButton('♥️ Source', callback_data='source')
         ], [
             InlineKeyboardButton('🏠 Home', callback_data='start'),
